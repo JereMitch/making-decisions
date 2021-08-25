c6d58e95b9cc63cc67334134deeb432a053c46eb
JereMitch <jere.mitch8@gmail.com>
2021-08-25 00:55:31 -0600

    finished all EXCEPT 12

--- lab.js
+++ lab.js
@@ -161,7 +161,7 @@ if (changeMyMind === true) {
 
 //CODE HERE
 
-console.log(changeMyMind)
+console.log(changeMyMind);
 
 ////////// PROBLEM 10 //////////
 
@@ -175,23 +175,22 @@ var myFavoriteNumbers = [4, 8, 12, 16, 20, 24];
 
 //Code Here
 
-let someNum = (myFavoriteNumbers[4])
-
+let someNum = myFavoriteNumbers[4];
 
 /* Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, 
 but there are not 7 elements in the array? Let's write some code to check for that.*/
 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, 
-//log 'There are not enough elements in this array' to the console. If the length is more than 7, 
-//reassign the value of 'someNum' to the value of the 7th element in the array. 
+// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7,
+//log 'There are not enough elements in this array' to the console. If the length is more than 7,
+//reassign the value of 'someNum' to the value of the 7th element in the array.
 //(Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
 
-if(myFavoriteNumbers.length >= 6){
-  console.log("There are not enough elements in this array")
-} else if(myFavoriteNumbers.length <= 6){
-myFavoriteNumbers.push(someNum[6])
+if (myFavoriteNumbers.length >= 6) {
+  console.log("There are not enough elements in this array");
+} else if (myFavoriteNumbers.length <= 6) {
+  myFavoriteNumbers.push(someNum[6]);
 }
 
 ////////// PROBLEM 11 //////////
@@ -200,20 +199,21 @@ myFavoriteNumbers.push(someNum[6])
 var listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. 
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3.
 //If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
 
-let number = []
+let number = [];
 
-for(let i = 0; i < listOfNumbers.length; i++) {
+for (let i = 0; i < listOfNumbers.length; i++) {
   console.log(listOfNumbers[i]);
-  if(listOfNumbers [i] % 3 === 0) {
-      number.push(listOfNumbers[i]);
+  if (listOfNumbers[i] % 3 === 0) {
+    number.push(listOfNumbers[i]);
   }
 }
-  console.log(number)
+console.log(number);
+
 ////////// PROBLEM 12 //////////
 // Do not edit the code below.
 var letters = ["A", "B", "C", "D", "E"];
@@ -224,6 +224,10 @@ var letters = ["A", "B", "C", "D", "E"];
 */
 
 //Code Here
+
+for(let i = 0; i < letters.length; i++) {
+  console.log(letters[i]);
+}
 
 ////////// Advanced Problems //////////
 
@@ -246,24 +250,27 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
-// switch (letterGrade) {
-//   case "A":
-//     console.log("The student is doing excellently.");
-//   case "B":
-//     console.log("The student is doing well.");
-//   case "C":
-//     console.log("The student is doing alright.");
-//   case "D":
-//     console.log("The student is not doing very well.");
-//   case "F":
-//     console.log("The student is failing.");
-//   default:
-//     console.log("Not and eligible grade.");
-// }
+switch (letterGrade) {
+  case "A":
+    console.log("The student is doing excellently.");
+  case "B":
+    console.log("The student is doing well.");
+  case "C":
+    console.log("The student is doing alright.");
+  case "D":
+    console.log("The student is not doing very well.");
+  case "F":
+    console.log("The student is failing.");
+  default:
+    console.log("Not and eligible grade.");
+}
 
 ////////// PROBLEM 14 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, 
+  if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, 
+  console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+  If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
 
   Your output should look like:
   1
@@ -286,3 +293,17 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for(let i = 1; i < 101; i++) {
+  if(i % 3 & i % 5 === 0) {
+    console.log("Devmountain")
+  } else if(i % 5 === 0) {
+    console.log("mountain") 
+  } else if (i % 3 === 0) {
+    console.log("Dev")
+  } else {
+    console.log(i)
+  }
+
+  
+}
